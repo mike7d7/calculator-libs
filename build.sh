cd06781d6433b77273f5f52164fdae9a73d9841a
mike7d7 <mike7d7@proton.me>
2024-11-30 17:05:39 -0600

    add xml2 to script

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ GMP_VERSION="6.3.0"
 MPFR_VERSION="4.2.1"
 XZ_VERSION="5.6.3"
 ICONV_VERSION="1.17"
+XML2_VERSION="2.13.5"
 
 ROOT_DIR=$(pwd)
 WORKER=$(getconf _NPROCESSORS_ONLN)
@@ -18,6 +19,7 @@ BUILD_DIR_GMP="build/android/gmp"
 BUILD_DIR_MPFR="build/android/mpfr"
 BUILD_DIR_XZ="build/android/xz"
 BUILD_DIR_ICONV="build/android/iconv"
+BUILD_DIR_XML2="build/android/xml2"
 LOG_FILE="$ROOT_DIR/$BUILD_DIR_OPENSSL/build.log"
 
 COLOR_GREEN="\033[38;5;48m"
@@ -791,4 +793,125 @@ for CURRENT_ARCH in "${TARGET_ARCHS[@]}"; do
 done
 
 echo -e "${COLOR_GREEN}iconv built successfully for all ARCH targets.${COLOR_END}"
+
+# xml2
+cd "$ROOT_DIR" || exit 1
+
+LOG_FILE="$ROOT_DIR/$BUILD_DIR_XML2/build.log"
+
+if [ -d "$BUILD_DIR_XML2/tar" ]; then
+  rm -rf "$BUILD_DIR_XML2/tar"
+fi
+if [ -d "$BUILD_DIR_XML2/src" ]; then
+  rm -rf "$BUILD_DIR_XML2/src"
+fi
+if [ -d "$BUILD_DIR_XML2/install" ]; then
+  rm -rf "$BUILD_DIR_XML2/install"
+fi
+
+if [ -f "$LOG_FILE" ]; then
+    rm "$LOG_FILE"
+    touch "$LOG_FILE"
+fi
+
+mkdir -p "$BUILD_DIR_XML2/tar"
+mkdir -p "$BUILD_DIR_XML2/src"
+mkdir -p "$BUILD_DIR_XML2/install"
+
+cd "$ROOT_DIR"
+echo "Downloading XML2..."
+curl -Lo "$BUILD_DIR_XML2/tar/xml2-$XML2_VERSION.tar.gz" "https://gitlab.gnome.org/GNOME/libxml2/-/archive/v$XML2_VERSION/libxml2-v$XML2_VERSION.tar.gz" >> "$LOG_FILE" 2>&1 || fail "Error Downloading xml2"
+echo "Uncompressing XML2..."
+tar xzf "$BUILD_DIR_XML2/tar/xml2-$XML2_VERSION.tar.gz" -C "$BUILD_DIR_XML2/src" || fail "Error Uncompressing XML2"
+cd "$BUILD_DIR_XML2/src/libxml2-v$XML2_VERSION"
+
+for CURRENT_ARCH in "${TARGET_ARCHS[@]}"; do
+    echo "Building XML2 for $CURRENT_ARCH build..."
+
+    make clean 1>& /dev/null || true
+
+    echo "-> Configuring XML2 for $CURRENT_ARCH build..."
+    case $CURRENT_ARCH in
+        armv7)
+            export HOST="arm-linux-androideabi"
+
+            export CC="armv7a-linux-androideabi16-clang"
+            export CXX="armv7a-linux-androideabi16-clang++"
+            export AR="arm-linux-androideabi-ar"
+            export AS="arm-linux-androideabi-as"
+            export LD="arm-linux-androideabi-ld"
+            export RANLIB="arm-linux-androideabi-ranlib"
+            export NM="arm-linux-androideabi-nm"
+            export STRIP="arm-linux-androideabi-strip"
+
+            export CFLAGS="--sysroot=$ANDROID_TOOLCHAIN/sysroot -Wl,--fix-cortex-a8 -fPIC -DANDROID -D__ANDROID_API__=16 -Os"
+            export CPPFLAGS="$CFLAGS"
+            export CXXFLAGS="$CFLAGS -fno-exceptions -fno-rtti"
+            export LDFLAGS="-Wl,--fix-cortex-a8"
+        ;;
+        arm64)
+            export HOST="aarch64-linux-android"
+
+            export CC="x86_64-linux-android21-clang"
+            export CXX="x86_64-linux-android21-clang++"
+            export AR="llvm-ar"
+            export AS="$CC"
+            export LD="ld"
+            export RANLIB="llvm-ranlib"
+            export NM="nm"
+            export STRIP="llvm-strip"
+
+            ./autogen.sh --host=$HOST --enable-static --without-python --with-lzma="$ROOT_DIR/$BUILD_DIR_XZ/install/xz/$CURRENT_ARCH/usr/local" >> "$LOG_FILE" 2>&1 || fail "-> Error Configuring XML2 for $CURRENT_ARCH"
+        ;;
+        x86)
+            export HOST="i686-linux-android"
+
+            export CC="i686-linux-android16-clang"
+            export CXX="i686-linux-android16-clang++"
+            export AR="i686-linux-android-ar"
+            export AS="i686-linux-android-as"
+            export LD="i686-linux-android-ld"
+            export RANLIB="i686-linux-android-ranlib"
+            export NM="i686-linux-android-nm"
+            export STRIP="i686-linux-android-strip"
+
+            export CFLAGS="--sysroot=$ANDROID_TOOLCHAIN/sysroot -fPIC -mtune=intel -mssse3 -mfpmath=sse -m32 -DANDROID -D__ANDROID_API__=16 -Os"
+            export CPPFLAGS="$CFLAGS"
+            export CXXFLAGS="$CFLAGS -fno-exceptions -fno-rtti"
+            export LDFLAGS=""
+        ;;
+        x86_64)
+            export HOST="x86_64-linux-android"
+
+            export CC="x86_64-linux-android21-clang"
+            export CXX="x86_64-linux-android21-clang++"
+            export AR="x86_64-linux-android-ar"
+            export AS="x86_64-linux-android-as"
+            export LD="x86_64-linux-android-ld"
+            export RANLIB="x86_64-linux-android-ranlib"
+            export NM="x86_64-linux-android-nm"
+            export STRIP="x86_64-linux-android-strip"
+
+            export CFLAGS="--sysroot=$ANDROID_TOOLCHAIN/sysroot -fPIC -mtune=intel -mssse3 -mfpmath=sse -m64 -DANDROID -D__ANDROID_API__=21 -Os"
+            export CPPFLAGS="$CFLAGS"
+            export CXXFLAGS="$CFLAGS -fno-exceptions -fno-rtti"
+            export LDFLAGS=""
+        ;;
+    esac
+
+    # sed -i '' -e 's~#define HAVE_STRDUP~//#define HAVE_STRDUP~g' configure
+    echo "-> Configured XML2 for $CURRENT_ARCH"
+
+    echo "-> Compiling XML2 for $CURRENT_ARCH..."
+    make -j "$WORKER" >> "$LOG_FILE" 2>&1 || fail "-> Error Compiling XML2 for $CURRENT_ARCH"
+    echo "-> Compiled XML2 for $CURRENT_ARCH"
+
+    echo "-> Installing XML2 for $CURRENT_ARCH to $ROOT_DIR/$BUILD_DIR_XML2/install/xml2/$CURRENT_ARCH..."
+    make install DESTDIR="$ROOT_DIR/$BUILD_DIR_XML2/install/xml2/$CURRENT_ARCH" >> "$LOG_FILE" 2>&1 || fail "-> Error Installing XML2 for $CURRENT_ARCH"
+    echo "-> Installed XML2 for $CURRENT_ARCH"
+
+    echo "Successfully built XML2 for $CURRENT_ARCH"
+done
+
+echo -e "${COLOR_GREEN}xml2 built successfully for all ARCH targets.${COLOR_END}"
 exit 0
