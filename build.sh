2ff8b4b98b651fbecca26d8c6d7ccb8e50151245
mike7d7 <mike7d7@proton.me>
2024-12-01 00:23:06 -0600

    add custom libmpfr.la to fix libgmp.la location

--- build.sh
+++ build.sh
@@ -547,6 +547,8 @@ for CURRENT_ARCH in "${TARGET_ARCHS[@]}"; do
 
     echo "-> Installing MPFR for $CURRENT_ARCH to $ROOT_DIR/$BUILD_DIR_MPFR/install/mpfr/$CURRENT_ARCH..."
     make install DESTDIR="$ROOT_DIR/$BUILD_DIR_MPFR/install/mpfr/$CURRENT_ARCH" >> "$LOG_FILE" 2>&1 || fail "-> Error Installing MPFR for $CURRENT_ARCH"
+    rm $ROOT_DIR/$BUILD_DIR_MPFR/install/mpfr/$CURRENT_ARCH/usr/local/lib/libmpfr.la
+    cp $ROOT_DIR/libmpfr.la $ROOT_DIR/$BUILD_DIR_MPFR/install/mpfr/$CURRENT_ARCH/usr/local/lib/libmpfr.la
     echo "-> Installed MPFR for $CURRENT_ARCH"
 
     echo "Successfully built MPFR for $CURRENT_ARCH"
